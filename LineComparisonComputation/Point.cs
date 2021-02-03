579e48ce522a3195c4c9165cc289e88600961926
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 12:07:19 +0530

    [vijay]FoundLineLength

---
+++ LineComparisonComputation/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonComputation
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public Point(int xValue, int yValue)
+        {
+            x = xValue;
+            y = yValue;
+        }
+    }
+}
