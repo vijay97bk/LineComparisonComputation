0e0170b9a5d77a81fd3c5fcf8da6093b8f005320
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 12:12:00 +0530

    [vijay]CheckEquality

--- LineComparisonComputation/Line.cs
+++ LineComparisonComputation/Line.cs
@@ -17,6 +17,24 @@ namespace LineComparisonComputation
         {
             return Math.Sqrt(Math.Pow(a.x - b.x, 2) + Math.Pow(a.y - b.y, 2)); ;
         }
+        public void Compare(Line l1)
+        {
+            if (l1.CalculateLength() == this.CalculateLength())
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
+        }
 
+        public void compareTo(Line l1)
+        {
+            double l1Length = l1.CalculateLength();
+            double thisLineLength = this.CalculateLength();
+            if (l1Length == thisLineLength)
+                Console.WriteLine("Lines are equal");
+            else if (l1Length > thisLineLength)
+                Console.WriteLine("This line is smaller than Second Line");
+            else
+                Console.WriteLine("This line is greater than Second Line");
+        }
     }
 }
