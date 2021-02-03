d7b593f0dd98dbf9b34a71b2f2a067384ab538e0
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 11:54:49 +0530

    first commit

---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparision computation");
+        }
+    }
+}
